6f1caf5ec724aafe2f3e6c64420e424d47629636
Tiago Barbosa <tiagosoundcar@gmail.com>
2021-11-04 16:13:27 -0300

    Alterando Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
           <p className="text-light">Aplicativo desenvolvido por <a href="https://www.linkedin.com/in/tiagoalvestec/" target="_blank" rel="noreferrer">Tiago Barbosa</a></p>
-          <p className="text-light"><strong>Semana Spring React</strong><br/>Evento promovido pela escola DevSuperior</p>
+          <p className="text-light"><>Spring Boot React Full Stack | 2021</></p>
         </div>
         </footer>
     );
