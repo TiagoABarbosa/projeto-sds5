34b88d68a1516feedea889c59dad3b46e7c4efe5
Tiago Barbosa <tiagosoundcar@gmail.com>
2021-11-04 10:48:48 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">Aplicativo desenvolvido por <a href="https://www.linkedin.com/in/tiagoalvestec/" target="_blank" rel="noreferrer">Tiago Barbosa</a></p>
+          <p className="text-light"><strong>Semana Spring React</strong><br/>Evento promovido pela escola DevSuperior</p>
+        </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
