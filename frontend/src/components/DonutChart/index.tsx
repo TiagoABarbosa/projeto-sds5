41da3250b815a4c128765db5a778977c446ee616
Tiago Barbosa <tiagosoundcar@gmail.com>
2021-11-08 00:59:21 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,11 +25,6 @@ const DonutChart = () => {
     });
   }, []);
 
-  //const mockData = {
-  //  series: [477138, 499928, 444867, 220426, 473088],
-  //  labels: ["Anakin", "Qui-Gon Jinn", "Palpatine", "Obi-Wan", "Padmé"],
-  //}
-
   const options = {
     legend: {
       show: true,
