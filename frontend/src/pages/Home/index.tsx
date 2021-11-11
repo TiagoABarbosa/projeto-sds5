31ba56ad92a708eade25994cc74be01d1a8d4224
Tiago Barbosa <tiagosoundcar@gmail.com>
2021-11-10 23:35:08 -0300

    atualizando projeto

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
         <div className="jumbotron">
           <h1 className="display-4">DSVendas</h1>
           <p className="lead">
-            Analise o desempenho das suas vendas por diferentes perspectivas</p>
+            Analise o desempenho das suas vendas por diferentes perspectivas.</p>
           <hr />
           <p>Esta aplicação consiste em exibir um Dashboard a partir de dados fornecidos por um backend construído com Spring Boot.</p>
           <Link className="btn btn-warning btn-lg=" to="/dashboard">
